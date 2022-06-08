ba920dac9e46638372b240671b55280b11c14895
Griffin Novetsky <griffinnovetsky@gmail.com>
2022-06-08 15:32:38 -0400

    Updates to Panel and Score

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -51,6 +51,7 @@ public class GamePanel extends JPanel implements Runnable {
         paddle1.draw(g);
         paddle2.draw(g);
         ball.draw(g);
+        score.draw(g);
     }
     public void move() {
         paddle1.move();
--- src/Score.java
+++ src/Score.java
@@ -6,14 +6,18 @@ import javax.swing.*;
 public class Score extends Rectangle {
     
     static int GAME_WIDTH;
-    static int GAME_HEIGTH;
+    static int GAME_HEIGHT;
     int player1;
     int player2;
 
     Score(int GAME_WIDTH, int GAME_HEIGHT) {
-
+        Score.GAME_WIDTH = GAME_WIDTH;
+        Score.GAME_HEIGHT = GAME_HEIGHT;
     }
     public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(new Font("Consolas", Font.PLAIN, 60));
 
+        g.drawLine(GAME_WIDTH / 2, 0, GAME_WIDTH / 2, GAME_HEIGHT);
     }
 }
