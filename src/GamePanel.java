3ff6f1a08233414761151072987c59d81438b232
Griffin Novetsky <griffinnovetsky@gmail.com>
2022-06-08 12:50:34 -0400

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -56,6 +56,21 @@ public class GamePanel extends JPanel implements Runnable {
     }
     public void run() {
         // game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
     }
     public class AL extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
