b4d330c53a8f85b76a65ef817ec529321e0c95f9
Griffin Novetsky <griffinnovetsky@gmail.com>
2022-06-08 12:58:12 -0400

    Update Paddle.java

--- src/Paddle.java
+++ src/Paddle.java
@@ -26,6 +26,10 @@ public class Paddle extends Rectangle {
 
     }
     public void draw(Graphics g) {
-
+        if (id == 1)
+            g.setColor(Color.blue);
+        else
+            g.setColor(Color.red);
+        g.fillRect(x, y, width, height);
     }
 }
